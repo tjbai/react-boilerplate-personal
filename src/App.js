eba689b0a3feb9a085a7aed4deb66a3322ae2efb
TJ Bai <tjianbai@gmail.com>
2022-06-06 14:15:16 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there!</p>
+      </header>
+    </div>
+  );
+
